0d8da8e26e25e9c7088a2eea9f4ea59b867b4620
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-13 02:43:30 +0900

    Checked react-canvas-polygons without typescript

---
+++ oyster_js/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from "react";
+import DrawCanvas from "./DrawCanvas";
+
+
+
+function App() {
+  const [points, setPoint] = useState();
+
+  const onChange = (data) => {
+    setPoint(data);
+    console.log(points);
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <DrawCanvas initialData={points} onChange={onChange} />
+    </div>
+  );
+}
+
+export default App;
