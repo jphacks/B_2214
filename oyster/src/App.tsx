8946024e364f0f35c67184ec6b7e9c7bacd07f3d
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-13 20:24:20 +0900

    Deleted js folder and cleaned up

--- oyster/src/App.tsx
+++ oyster/src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from "react";
-import DrawCanvas from "./DrawCanvas";
+import { useState } from "react";
+import DrawCanvas from "./components/DrawCanvas";
 
 function App() {
   const [points, setPoint] = useState();
@@ -11,23 +9,9 @@ function App() {
     setPoint(data);
     console.log(points);
   };
-  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <DrawCanvas initialData={points} onChange={onChange} />
     </div>
   );
