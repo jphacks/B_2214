397ae4d743827c9641d023abd6d0f74803eb57e1
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-13 23:06:35 +0900

    Added area calculation of selected polygon

--- oyster/src/App.js
+++ oyster/src/App.js
@@ -4,15 +4,23 @@ import DrawCanvas from "./components/DrawCanvas";
 
 function App() {
   const [points, setPoint] = useState();
+  const [pixelArea, setPixelArea] = useState(0);
+  var area = require('area-polygon')
 
   const onChange = (data) => {
     setPoint(data);
     console.log(points);
+    // calculate area
+    if(points&&Object.keys(points).length>1&&points[String(Object.keys(points)[1])].length>2){
+      setPixelArea(area(points[String(Object.keys(points)[1])]))
+      console.log(area(points[String(Object.keys(points)[1])]))
+    }
   };
 
   return (
     <div className="App">
       <DrawCanvas initialData={points} onChange={onChange} />
+      <p>{pixelArea}</p>
     </div>
   );
 }
