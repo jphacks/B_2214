559d130b4d71549c2e7b685987644d9ec522ee89
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-14 20:51:45 +0900

    Checked local docker container works

---
+++ api/app/main.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+
+import torch
+from torch import nn
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import numpy as np
+from PIL import Image
+import urllib
+
+app = Flask(__name__)
+CORS(app)
+
+# Load model
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+model.eval()
+print("Loaded model")
+
+# Check for GPU
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+model = model.to(device)
+
+# Preprocessing and postprocessing
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Health check route
+@app.route("/isalive")
+def is_alive():
+    print("/isalive request")
+    status_code = Response(status=200)
+    return status_code
+
+
+# Predict route
+@app.route("/predict", methods=["POST"])
+def predict():
+    print("/predict request")
+    req_json = request.get_json()
+    url = req_json["image"]
+    print(url)
+    filename = "test.png"
+    try: urllib.URLopener().retrieve(url, filename)
+    except: urllib.request.urlretrieve(url, filename)
+    input_image = Image.open(filename)
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+    # move the input and model to GPU for speed if available
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+        model.to('cuda')
+
+    with torch.no_grad():
+        output = model(input_batch)
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    # Read the categories
+    with open("imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+    # Show top categories per image
+    top5_prob, top5_catid = torch.topk(probabilities, 5)
+    result = []
+    for i in range(top5_prob.size(0)):
+        result.append([categories[top5_catid[i]], top5_prob[i].item()])
+    print(result)
+
+    return jsonify({
+        "predictions": result
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
--- oyster/src/components/TopPage/index.js
+++ oyster/src/components/TopPage/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DrawCanvasSection from './DrawCanvasSection';
 
 const TopPage = () => {
@@ -7,6 +7,30 @@ const TopPage = () => {
       setPoint(data);
       console.log(points);
     };
+
+    useEffect(() => {
+      fetch('http://localhost:8080/isalive', {method: 'GET'})
+      .then(res => {
+          console.log(res)
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"image": "https://github.com/pytorch/hub/raw/master/images/dog.jpg"})
+      };
+      fetch('http://localhost:8080/predict', requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  },[])
+
 
   return (
     <>
