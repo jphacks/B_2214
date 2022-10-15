849b80f0b8e46c248b74e566b368e45cb576a69e
Kosei Nakamura <nakamura-3k559111@g.ecc.u-tokyo.ac.jp>
2022-10-15 13:57:01 +0900

    set mantine theme

--- oyster/src/index.js
+++ oyster/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import App from './pages/App';
 import TopPage from './pages/TopPage';
-// import App from './pages/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    {/* <App> */}
-    <TopPage />
-    {/* </App> */}
+    <App>
+      <TopPage />
+    </App>
   </>,
 );
