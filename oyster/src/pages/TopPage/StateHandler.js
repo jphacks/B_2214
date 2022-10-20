5f848fda286535eb5190216c3fe406453cd8f35d
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-21 01:31:55 +0900

    Added Footer

--- oyster/src/pages/TopPage/StateHandler.js
+++ oyster/src/pages/TopPage/StateHandler.js
@@ -1,8 +1,9 @@
-import { Grid, createStyles, Container, useMantineTheme, Header, AppShell } from '@mantine/core';
+import { Grid, createStyles, Container, useMantineTheme, Header, AppShell, Footer } from '@mantine/core';
 
 import CalculateSection from '../../components/CalculateSection';
 import DrawCanvasSection from '../../components/DrawCanvasSection';
 import DropImageSection from '../../components/DropImageSection';
+import { FooterLinks } from '../../components/FooterLinks/FooterLinks';
 import { Logo } from '../../components/Logo/logo';
 import Predict from '../../components/Predict';
 import Result from '../../components/Result';
@@ -38,10 +39,14 @@ const StateHandler = () => {
   return (
     <>
       <AppShell
+        fixed={false}
         padding="md"
         header={<Header height={60} p="xs">
           <Logo></Logo>
         </Header>}
+        footer={<Footer height={175} >
+          <FooterLinks links = {[{link:'https://github.com/jphacks/B_2214',label:'github repo'}]}></FooterLinks>
+          </Footer>}
         styles={(theme) => ({
           main: { backgroundColor: theme.colors.gray[0] },
         })}
