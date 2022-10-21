dcc5c31d6c0697e825dedf0eb8c1b3db8ca9ceb5
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-21 13:28:15 +0900

    Bug fix for training locally

--- machine_learning/semantic_segmentation/utils/create_annotation.py
+++ machine_learning/semantic_segmentation/utils/create_annotation.py
@@ -12,6 +12,7 @@ def main():
     with (annotation_path).open('r') as f:
         annotation = json.load(f)
 
+    count = 0
     for anno in annotation:
         img = np.asarray(Image.open(data_dir / anno['External ID']))
         black_img = np.full_like(img, 0)
@@ -20,7 +21,11 @@ def main():
             anno_list.append([int(dot['x']), int(dot['y'])])
         mask_poly = np.array(anno_list)
         masked_img = cv2.fillPoly(black_img, [mask_poly], (255, 0, 0))
-        Image.fromarray(masked_img).save(save_img_dir / anno['External ID'])
+        if len(masked_img.shape) == 3:
+            Image.fromarray(masked_img,mode='RGB').save(save_img_dir / anno['External ID'])
+        else:
+            count += 1
+    print(count)
 
 
 if __name__ == '__main__':
