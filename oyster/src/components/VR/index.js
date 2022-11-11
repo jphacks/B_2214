47c0e8ea9fe93f3e81ae8252d6ffa3985884223f
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-11-12 04:37:09 +0900

    joystick control fix attempt

--- oyster/src/components/VR/index.js
+++ oyster/src/components/VR/index.js
@@ -38,33 +38,11 @@ const VR = () => {
   return (
     <div className={classes.root}>
       <div className={classes.vrcontainer}>
-        <a-scene>
+        <a-scene embedded>
             {/* <a-text value={pageName} position="0 25 -100" width="500" align="center" zOffset="1">{console.log(props.pageName)}</a-text> */}
             <a-entity environment="preset: default" position="0 -3 0"></a-entity>
             {/* <a-entity light=" intensity: 1.5" position="0 100 0"></a-entity> */}
             <a-box color="red" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
-
-            <a-entity id="rig">
-                {/* <a-entity id="camera" camera wasd-controls look-controls position="0 1.6 0"></a-entity>
-                <a-entity id="left-hand"
-                    oculus-touch-controls="hand: left"
-                    vive-controls="hand: left"
-                    microsoft-motion-controls="hand: left"
-                    daydream-controls="left"
-                    gearvr-controls="left"
-                ></a-entity>
-                <a-entity id="right-hand"
-                    oculus-touch-controls="hand: right"
-                    vive-controls="hand: right"
-                    microsoft-motion-controls="hand: right"
-                    daydream-controls="right"
-                    gearvr-controls="right"
-                    oculus-thumbstick-controls
-                ></a-entity> */}
-              <a-camera position="0 1.6 0"></a-camera>
-              <a-entity oculus-touch-controls="hand: left" ></a-entity>
-              <a-entity oculus-touch-controls="hand: right" oculus-thumbstick-controls></a-entity>
-            </a-entity>
 
             <a-entity
               // gltf-model="test.gltf"
@@ -73,6 +51,26 @@ const VR = () => {
               scale="0.05 0.05 0.05"
               crossOrigin="anonymous"
             ></a-entity>
+
+            <a-entity id="player" movement-controls="fly: false;">
+                <a-entity id="camera" camera wasd-controls look-controls position="0 1.6 0"></a-entity>
+                <a-entity id="left-hand"
+                    hand-controls="hand: left;"
+                    oculus-touch-controls="hand: left"
+                    vive-controls="hand: left"
+                    microsoft-motion-controls="hand: left"
+                    daydream-controls="left"
+                    gearvr-controls="left"
+                ></a-entity>
+                <a-entity id="right-hand"
+                    hand-controls="hand: right;"
+                    oculus-touch-controls="hand: right"
+                    vive-controls="hand: right"
+                    microsoft-motion-controls="hand: right"
+                    daydream-controls="right"
+                    gearvr-controls="right"
+                ></a-entity>
+            </a-entity>
         </a-scene>
       </div>
     </div>
