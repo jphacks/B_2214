5da41a7456f72dcff056139f851c4305086b8b43
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-11-12 15:56:39 +0900

    Changed 3d model position

--- oyster/src/components/VR/index.js
+++ oyster/src/components/VR/index.js
@@ -42,10 +42,9 @@ const VR = () => {
       <div className={classes.vrcontainer}>
         <a-scene embedded>
             <a-entity environment="preset: default" position="0 -3 0"></a-entity>
-
             <a-entity
               gltf-model={model?.prediction[0].url}
-              position= "0 2 -5"
+              position= "5 0 -15"
               scale = {`${scale} ${scale} ${scale}`}
               crossOrigin="anonymous"
             ></a-entity>
